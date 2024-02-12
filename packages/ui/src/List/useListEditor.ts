1b070fa39ace690ffd10853d6e11132ca6f6e81c
Andy Phillipson <ajp@mindspring.com>
2024-02-12 17:13:45 -0500

    Add ability for list editor to handle multiple list on a single screen, add reset editor when navigating away from screen

--- packages/ui/src/List/useListEditor.ts
+++ packages/ui/src/List/useListEditor.ts
@@ -1,11 +1,24 @@
-import { useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 import type { ListItemMethods } from './ListItem';
+import { useIsFocused } from '@react-navigation/native';
 
 export const useListEditor = () => {
-  const liRef = useRef<ListItemMethods[]>([]);
+  const isFocused = useIsFocused();
+
+  // References are indexed by group and then ordinal index in the group.
+  // Groups allow multiple lists on the same screen all responding to the
+  // single editor instance.
+  const liRef = useRef<Record<string, ListItemMethods[]>>({});
   const [listEditSwipeEnabled, setListEditSwipeEnabled] = useState(false);
   const [listEditModeEnabled, setListEditModeEnabled] = useState(false);
+
+  // Reset the editor when the screen loses focus (navigate away).
+  useEffect(() => {
+    !isFocused && resetEditor();
+    setListEditModeEnabled(false);
+    setListEditSwipeEnabled(false);
+  }, [isFocused]);
 
   const onEdit = () => {
     if (listEditSwipeEnabled) {
@@ -19,9 +32,13 @@ export const useListEditor = () => {
     }
   };
 
-  const onSwipeableWillOpen = (index: number) => {
-    liRef.current.forEach((li, i) => {
-      i !== index && li.resetEditor();
+  const onSwipeableWillOpen = (group: string, index: number) => {
+    Object.keys(liRef.current).forEach(g => {
+      (liRef.current[g] as ListItemMethods[]).forEach((li, i) => {
+        console.log(g, i);
+        console.log(group, index);
+        !(g === group && i === index) && li.resetEditor();
+      });
     });
 
     // Need to allow the editor resets to run first.
@@ -34,15 +51,30 @@ export const useListEditor = () => {
     setListEditSwipeEnabled(false);
   };
 
-  const addToEditor = (ref: ListItemMethods | null, index: number) => {
+  // Add the reference to the editor by group. The group is used to allow
+  // multiple lists on the same screen, each list should have a different
+  // group name.
+  const addToEditor = (
+    ref: ListItemMethods | null,
+    group: string,
+    index: number,
+  ) => {
     if (ref) {
-      liRef.current[index] = ref;
+      if (!liRef.current[group]) {
+        liRef.current = {
+          ...liRef.current,
+          [group]: [],
+        };
+      }
+      liRef.current[group][index] = ref;
     }
   };
 
   const resetEditor = () => {
-    liRef.current.forEach(li => {
-      li.resetEditor();
+    Object.keys(liRef.current).forEach(group => {
+      liRef.current[group].forEach(li => {
+        li.resetEditor();
+      });
     });
   };
 
