7e04aa084f16115e2ed3921fea68204add14c777
Andy Phillipson <ajp@mindspring.com>
2024-07-29 18:27:29 -0400

    Fix image selection result

--- packages/ui/src/lib/imageSelect.ts
+++ packages/ui/src/lib/imageSelect.ts
@@ -46,66 +46,28 @@ export const selectImage = (args: {
     multiple: multiSelect,
     mediaType,
   })
-    .then(image => {
-      if (Array.isArray(image))
-        onSuccess([
-          {
-            height: image.height,
+    .then(result => {
+      const selections = Array.isArray(result) ? result : [result];
+      onSuccess(
+        selections.map(i => {
+          return {
+            height: i.height,
             metadata: {
-              created: image.creationDate,
-              exif: image.exif,
-              modified: image.modificationDate,
+              created: i.creationDate,
+              exif: i.exif,
+              modified: i.modificationDate,
             },
-            mimeType: image.mime,
-            name: image.filename || '',
-            size: image.size,
+            mimeType: i.mime,
+            name: i.filename || '',
+            size: i.size,
             type: mediaType,
-            uri: image.path,
-            width: image.width,
-          },
-        ]);
+            uri: i.path,
+            width: i.width,
+          };
+        }),
+      );
     })
     .catch(() => {
       onError && onError();
     });
-  // ImagePicker.launchImageLibrary(
-  //   {
-  //     mediaType,
-  //     selectionLimit: multiSelect ? 0 : 1,
-  //     assetRepresentationMode: 'current',
-  //   },
-  //   async response => {
-  //     try {
-  //       if (response.assets && response.assets[0]?.uri) {
-  //         onSuccess(response.assets);
-  //       } else if (response.errorCode) {
-  //         const msg = `Image select error: ${response.errorCode} - ${response.errorMessage}`;
-  //         if (response.errorCode.includes('permission')) {
-  //           Alert.alert(
-  //             'Permission Denied',
-  //             "You don't have permission to access your photo library.\n\nYou can grant permission in your Settings app.",
-  //             [{ text: 'OK' }],
-  //             { cancelable: false },
-  //           );
-  //         } else {
-  //           Alert.alert(
-  //             'Image Selection Error',
-  //             'An error occurred while accessing your photo library. Please try again.',
-  //             [{ text: 'OK' }],
-  //             { cancelable: false },
-  //           );
-  //           log.error(msg);
-  //         }
-  //         throw new Error(msg);
-  //       }
-  //       // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  //     } catch (e: any) {
-  //       onError && onError();
-  //     }
-  //   },
-  //   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  // ).catch((e: any) => {
-  //   log.error(e.message);
-  //   onError && onError();
-  // });
 };
